f980465db532aed16a80e786e7e65d5ccc1247fb
mys1024 <mys.1024@outlook.com>
2023-08-04 00:19:42 +0800

    chore: update deps

--- unocss.config.ts
+++ unocss.config.ts
@@ -4,7 +4,6 @@ import {
   presetIcons,
   presetTypography,
   presetUno,
-  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
@@ -21,13 +20,6 @@ export default defineConfig({
       warn: true,
     }),
     presetTypography(),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
-    }),
   ],
   transformers: [
     transformerDirectives(),
