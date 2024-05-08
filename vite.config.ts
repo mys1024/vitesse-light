80a28f410f494e15d5573f446cd640e2eb65d361
mys1024 <mys.1024@outlook.com>
2024-05-09 00:50:45 +0800

    feat: rm PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import ResolveAlias from 'vite-plugin-easy-resolve-alias'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Unocss from 'unocss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import VueI18n from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
@@ -17,34 +16,6 @@ export default defineConfig({
     Pages({ dirs: 'src/pages' }),
     // https://github.com/antfu/unocss
     Unocss(),
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: 'Vitesse Light',
-        short_name: 'Vitesse Light',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
     // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
     VueI18n({
       include: [path.resolve(__dirname, 'locales/**')],
